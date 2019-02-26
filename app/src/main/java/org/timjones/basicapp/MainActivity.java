8bd26d10b2369da5a478913c5cc32c261d20739c
Tim Jones <drkanukie@gmail.com>
2019-02-26 21:56:25 +0000

    First commit

--- app/src/main/java/org/timjones/basicapp/MainActivity.java
+++ app/src/main/java/org/timjones/basicapp/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // test comment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
